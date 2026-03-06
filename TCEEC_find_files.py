a20d974ad3d9c962a30a0a44b8c1143d0aa1dbe8
Akseli Kettunen <akseli.kettunen@helsinki.fi>
2026-03-06 11:33:45 +0200

    Added function to add only originals in TCEECE and to get texts by paragraph.

--- TCEEC_find_files.py
+++ TCEEC_find_files.py
@@ -26,7 +26,31 @@ def find_original_and_c7(path: str):
     return file_pairs_d
 
 
+def find_original(path: str):
+    originals_d = {}
+
+    for entry in os.listdir(path):
+        full_path = os.path.join(path, entry)
+
+        orig_xml = ""
+
+        if os.path.isdir(full_path):
+
+            for file in os.listdir(full_path + "/orig/"):
+                if file.endswith(".xml"):
+                    orig_xml = full_path + "/orig/" + file
+
+            originals_d[entry] = {}
+            originals_d[entry]["orig"] = orig_xml
+
+    return originals_d
+
+
 if __name__ == "__main__":
+    res = find_original("./data/TCEECE/tceece-collections/")
+    print(res)
+    """
     res = find_original_and_c7("./data/TCEECE/tceece-collections/")
     print(res)
     print(res["FGIBBON"]["orig"])
+    """
--- TCEEC_find_records.py
+++ TCEEC_find_records.py
@@ -10,15 +10,84 @@ def find_root(collection_xml: str):
     return root
 
 
-def get_tei_collection_id(collection_xml: str) -> str:
-    root = find_root(collection_xml=collection_xml)
+def get_tei_collection_id(root) -> str:
     xml_id = root.attrib["{http://www.w3.org/XML/1998/namespace}id"]
 
     return xml_id
 
 
+def find_TEIs_from_root(root):
+    tei_list = root.findall("TEI")
+
+    return tei_list
+
+
+def generate_tei_dict(tei_list):
+    tei_dict = {}
+
+    ns = {"letterID": "{http://www.w3.org/XML/1998/namespace}id"}
+
+    for tei in tei_list:
+        letterid = tei.get(ns["letterID"])
+        # letter_n = int(letterid[-3:])
+
+        tei_dict[letterid] = tei
+
+    return tei_dict
+
+
+def paragraph_to_text(p_el):
+    parts = []
+
+    if p_el.text:
+        parts.append(p_el.text)
+    for child in p_el:
+        parts.append(paragraph_to_text(child))
+        if child.tail:
+            parts.append(child.tail)
+
+    return "".join(parts)
+
+
+def get_tei_text(tei):
+    textElement = tei.find("text")
+
+    tei_text = ""
+
+    if textElement is not None:
+        for paragraph in textElement.findall("p"):
+            p_text = paragraph_to_text(paragraph)
+            tei_text += p_text + "\n"
+
+    return tei_text
+
+
+def get_tei_text_by_paragraph(tei):
+    textElement = tei.find("text")
+
+    tei_text_by_p = []
+
+    if textElement is not None:
+        for paragraph in textElement.findall("p"):
+            p_text = paragraph_to_text(paragraph)
+            tei_text_by_p.append(p_text)
+
+    return tei_text_by_p
+
+
 if __name__ == "__main__":
+
+    root = find_root("./data/F2FLEMIN.xml")
+    collection_id = get_tei_collection_id(root)
+    tei_list = find_TEIs_from_root(root)
+    tei_dict = generate_tei_dict(tei_list)
+    flemin2_001 = tei_dict["FLEMIN2_001"]
+
+    tei_texts = get_tei_text_by_paragraph(flemin2_001)
+    print(tei_texts)
+    """
     path = "./data/TCEECE/tceece-collections/"
     res = find_original_and_c7(path)
     print(res)
     print(res["FGIBBON"]["orig"])
+    """
