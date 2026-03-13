aad8ccdcd5b585efbfe6470612c9e6b49eb208cd
Akseli Kettunen <akseli.kettunen@helsinki.fi>
2026-03-13 10:53:19 +0200

    Moved over text algnment to own module.

--- TCEEC_find_records.py
+++ TCEEC_find_records.py
@@ -1,6 +1,5 @@
 import xml.etree.ElementTree as ET
-
-from TCEEC_find_files import find_original_and_c7
+from align_parallel_texts import align_parallel_texts
 
 
 def find_root(collection_xml: str):
@@ -29,8 +28,6 @@ def generate_tei_dict(tei_list):
 
     for tei in tei_list:
         letterid = tei.get(ns["letterID"])
-        # letter_n = int(letterid[-3:])
-
         tei_dict[letterid] = tei
 
     return tei_dict
@@ -75,17 +72,77 @@ def get_tei_text_by_paragraph(tei):
     return tei_text_by_p
 
 
-if __name__ == "__main__":
+def create_dict(aligned_words):
+    res = {}
 
+    for pair in aligned_words:
+        if pair[1] not in res:
+            res[pair[1]] = 0
+
+        res[pair[1]] += 1
+
+    return res
+
+
+if __name__ == "__main__":
     root = find_root("./data/F2FLEMIN.xml")
     collection_id = get_tei_collection_id(root)
     tei_list = find_TEIs_from_root(root)
     tei_dict = generate_tei_dict(tei_list)
     flemin2_001 = tei_dict["FLEMIN2_001"]
 
-    tei_texts = get_tei_text_by_paragraph(flemin2_001)
-    print(tei_texts)
+    tei_texts1 = get_tei_text_by_paragraph(flemin2_001)
+    # print(tei_texts1[2])
+    # print()
+
+    root = find_root("./data/CEEC-400/CEECE-xml/FFLEMIN2.xml")
+    collection_id = get_tei_collection_id(root)
+    tei_list = find_TEIs_from_root(root)
+    tei_dict = generate_tei_dict(tei_list)
+    flemin2_001 = tei_dict["FLEMIN2_001"]
+
+    tei_texts2 = get_tei_text_by_paragraph(flemin2_001)
+    # print(tei_texts2[2])
+    #
+
+    res = align_parallel_texts(tei_texts1[2], tei_texts2[2])
+    for thing in res:
+        print(thing)
+        print()
     """
+
+    #
+    orig = tei_texts2[2]
+    norm = tei_texts1[2]
+
+    re_a, re_b = align_parallel_texts(orig, norm)
+
+    print(re_a)
+    print(re_b)
+    print(len(re_a), len(re_b))
+    print(re_a[8], re_b[8])
+    print(word_cost(re_a[8], re_b[8]))
+    res = align_words(re_a, re_b)
+
+    for item in res:
+        if item[0] != item[1]:
+            print(item)
+
+    print()
+    d = create_dict(res)
+    print(json.dumps(d, indent=4))
+    """
+    """
+
+    a = tokenize_text(orig)
+    b = tokenize_text(norm)
+    print(len(a), len(b))
+    print(a[9], b[9])
+
+    print(word_cost(a[9], b[9]))
+    print(res[8])
+    print(align_strings(res[8][0], res[8][1]))
+
     path = "./data/TCEECE/tceece-collections/"
     res = find_original_and_c7(path)
     print(res)
---
+++ align_parallel_texts.py
@@ -0,0 +1,175 @@
+import re
+import Levenshtein
+import numpy as np
+from nltk.tokenize import word_tokenize, sent_tokenize
+
+
+def align_parallel_texts(source_text: str, target_text: str):
+    src_text_sent_tokenized = sent_tokenize_text(source_text)
+    target_text_sent_tokenized = sent_tokenize_text(target_text)
+
+    print(
+        f"n of sents in src and tgt: {len(src_text_sent_tokenized)}, {len(target_text_sent_tokenized)}"
+    )
+
+    a = word_tokenize_from_sent_list(src_text_sent_tokenized)
+    b = word_tokenize_from_sent_list(target_text_sent_tokenized)
+
+    aligned_sents = align_sents(a, b)
+    aligned_by_sents_and_words = align_pairs_of_sents(aligned_sents)
+    return aligned_by_sents_and_words
+
+
+def word_tokenize_text(text: str):
+    tokenized_text = word_tokenize(text, language="english")
+
+    return tokenized_text
+
+
+# \w+\~+\w*\~?|\'?\w+\'?\w+|\w
+def re_word_tokenize_text(text: str) -> list[str]:
+    return re.findall(r"\w+\~+\w*\~?|\'?\w+\'?\w+|\w", text)
+
+
+def sent_tokenize_text(text: str):
+    res = sent_tokenize(text, language="english")
+    return res
+
+
+def word_tokenize_from_sent_list(sent_list: list):
+    res = []
+    for sent in sent_list:
+        res.append(re_word_tokenize_text(sent))
+
+    return res
+
+
+def align_sents(sent_list_a, sent_list_b):
+    """
+    if len(sent_list_a) != len(sent_list_b):
+        print("Warning! Mismatch in number of sentences.")
+        return None
+    """
+
+    res = []
+    for i in range(len(sent_list_a)):
+        res.append((sent_list_a[i], sent_list_b[i]))
+
+    return res
+
+
+# LLM Generated
+def word_cost(w1, w2):
+    return Levenshtein.distance(w1, w2) / max(len(w1), len(w2))
+
+
+# LLM Generated. Implements Needleman–Wunsch alignment
+def align_words(seq1, seq2):
+
+    n, m = len(seq1), len(seq2)
+
+    dp = [[0] * (m + 1) for _ in range(n + 1)]
+
+    gap_cost = 1
+
+    for i in range(1, n + 1):
+        dp[i][0] = i * gap_cost
+
+    for j in range(1, m + 1):
+        dp[0][j] = j * gap_cost
+
+    for i in range(1, n + 1):
+        for j in range(1, m + 1):
+
+            sub_cost = word_cost(seq1[i - 1], seq2[j - 1])
+
+            dp[i][j] = min(
+                dp[i - 1][j] + gap_cost,
+                dp[i][j - 1] + gap_cost,
+                dp[i - 1][j - 1] + sub_cost,
+            )
+
+    # backtrack
+
+    i, j = n, m
+    alignment = []
+
+    while i > 0 or j > 0:
+
+        if i > 0 and dp[i][j] == dp[i - 1][j] + gap_cost:
+            alignment.append((seq1[i - 1], None))
+            i -= 1
+
+        elif j > 0 and dp[i][j] == dp[i][j - 1] + gap_cost:
+            alignment.append((None, seq2[j - 1]))
+            j -= 1
+
+        else:
+            alignment.append((seq1[i - 1], seq2[j - 1]))
+            i -= 1
+            j -= 1
+
+    alignment.reverse()
+
+    return alignment
+
+
+# LLM generated
+# Not used. string alignment not useful as far as I can see (Akseli)
+def align_strings(a, b):
+    if a is None or b is None:
+        return None
+
+    n, m = len(a), len(b)
+    dp = np.zeros((n + 1, m + 1))
+
+    for i in range(n + 1):
+        dp[i][0] = i
+    for j in range(m + 1):
+        dp[0][j] = j
+
+    for i in range(1, n + 1):
+        for j in range(1, m + 1):
+            cost = 0 if a[i - 1] == b[j - 1] else 1
+            dp[i][j] = min(
+                dp[i - 1][j] + 1,  # deletion
+                dp[i][j - 1] + 1,  # insertion
+                dp[i - 1][j - 1] + cost,  # substitution
+            )
+
+    # backtrack
+    i, j = n, m
+    alignment = []
+
+    while i > 0 or j > 0:
+        if i > 0 and dp[i][j] == dp[i - 1][j] + 1:
+            alignment.append((a[i - 1], None))
+            i -= 1
+        elif j > 0 and dp[i][j] == dp[i][j - 1] + 1:
+            alignment.append((None, b[j - 1]))
+            j -= 1
+        else:
+            alignment.append((a[i - 1], b[j - 1]))
+            i -= 1
+            j -= 1
+
+    alignment.reverse()
+
+    return alignment
+
+
+def align_pairs_of_sents(aligned_sents):
+    res = []
+    for pair in aligned_sents:
+        res.append(align_words(pair[0], pair[1]))
+
+    return res
+
+
+if __name__ == "__main__":
+    a = "A university has said it was recently launched new institute could put a Hampshire city at the forefront of the UK space industry. The Southampton Space Institute, which belongs to the University of Southampton, will support government plans to develop Britain's fast-growing space sector, worth £17bn every year. It brings together the expertise of world-renowned research groups and facilities to drive the development of new space technology and conversations around policy and space sustainability. Inaugural director Prof Matt Middleton said the university has taught thousands of students about aircraft and satellite design since 1959."
+    b = "A univesity has said 'twas recently launch'd new institute cou'd put a Hampshire city at the forefront of the UK space industry. The Southampton Space Institute, whch belongs to the University of Southampton, will support government plans to develop Britains fast-growing space sector, worth £17bn every year. It brings together the expertise of world-renowned research groups and facilities to drive the development of new space technology and conversations around policy and space sustainability. Inaugural director Prof Matt Middleton sayd the university has taught thousands of students about aircraft and satellite design since 1959."
+    res = align_parallel_texts(a, b)
+    for thing in res:
+        print(thing)
+        print()
